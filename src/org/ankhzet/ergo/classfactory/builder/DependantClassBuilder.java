e92b4d8ed9345b961da433272d04ceecac5c01b1
AnkhZet <ankhzet@gmail.com>
2015-07-09 19:20:17 +0300

    some ghost conflicts

---
+++ src/org/ankhzet/ergo/classfactory/builder/DependantClassBuilder.java
@@ -0,0 +1,21 @@
+
+package org.ankhzet.ergo.classfactory.builder;
+
+/**
+ *
+ * @author Ankh Zet (ankhzet@gmail.com)
+ * @param <Type> Class to produce
+ * @param <Dependency> Class, builder is dependent of
+ */
+public class DependantClassBuilder<Type, Dependency> extends ClassBuilder<Type> {
+  Dependency dependency;
+
+  public DependantClassBuilder(Dependency dependency) {
+    this.dependency = dependency;
+  }
+
+  public Dependency getDependency() {
+    return dependency;
+  }
+
+}
