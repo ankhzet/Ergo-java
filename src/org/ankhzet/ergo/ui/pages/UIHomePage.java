485e0e555344414a566b3b5c173b5065e9d681e1
ankhzet <ankhzet@gmail.com>
2015-07-29 20:04:40 +0300

    Reading now can be resumed from bookmark

--- src/org/ankhzet/ergo/ui/pages/UIHomePage.java
+++ src/org/ankhzet/ergo/ui/pages/UIHomePage.java
@@ -7,6 +7,10 @@ import org.ankhzet.ergo.ui.pages.readerpage.reader.Reader;
 import org.ankhzet.ergo.manga.chapter.Chapter;
 import org.ankhzet.ergo.classfactory.annotations.DependenciesInjected;
 import org.ankhzet.ergo.classfactory.annotations.DependencyInjection;
+import org.ankhzet.ergo.manga.Bookmark;
+import org.ankhzet.ergo.manga.Manga;
+import org.ankhzet.ergo.ui.xgui.XButton;
+import org.ankhzet.ergo.ui.xgui.XControls;
 import org.ankhzet.ergo.ui.xgui.XPathFilePicker;
 
 /**
@@ -17,6 +21,9 @@ public class UIHomePage extends UIPage {
 
   static final String kLoadChapterAction = "load";
   static final String kLoadChapterLabel = "Load chapter";
+
+  static final String kContinueAction = "continue";
+  static final String kContinueLabel = "Continue";
 
   @DependencyInjection
   XPathFilePicker picker;
@@ -34,12 +41,36 @@ public class UIHomePage extends UIPage {
     hud.putActionAtLeft(kLoadChapterLabel, registerAction(kLoadChapterAction, action -> {
       loadChapter();
     }).enabledAs(action -> {
-      if ((!picker.hasSelected() || reader.isBusy()))
+      if (reader.isBusy())
         return false;
       
-      Chapter c = new Chapter(picker.getSelectedPath());
-      return c.valid();
+      return pickedChapter().valid();
     }));
+    
+    hud.putActionAtLeft(kContinueLabel, registerAction(kContinueAction, action -> {
+      Chapter c = new Chapter(picker.getSelectedPath());
+      Manga manga = new Manga(c.getMangaFolder());
+      Bookmark bookmark = manga.lastBookmark();
+      Chapter chapter = Chapter.chapterFromBookmark(c.toPath().getParent(), bookmark);
+      Chapter next = chapter.seekChapter(true);
+      if (next == null)
+        next = chapter;
+      ui.navigateTo(UIReaderPage.class, next);
+    })).enabledAs(action -> {
+      Chapter c = pickedChapter();
+      Manga manga = new Manga(c.getMangaFolder());
+      Bookmark b = manga.lastBookmark();
+      XButton cntButton = (XButton) hud.getControl(action);
+      if (cntButton != null) {
+        if (b != null)
+          cntButton.setCaption("Continue from: " + b.path(null).toString());
+        else
+          cntButton.setCaption(kContinueLabel);
+      }
+      return b != null;
+    });
+    
+    hud.putSpacer(XControls.AREA_LEFT);
     
     hud.putActionAtLeft("Filter duplicates", registerAction("dups", action -> {
       ui.navigateTo(UIDuplicatesPage.class, picker.getSelectedPath());
@@ -48,6 +79,10 @@ public class UIHomePage extends UIPage {
     });
     
     hud.add(picker);
+  }
+  
+  Chapter pickedChapter() {
+    return new Chapter(picker.getSelectedPath());
   }
 
   public boolean loadChapter() {
