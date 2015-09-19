1a51f67cd208f2f62ed0d6b1cb30907c4210d922
ankhzet <ankhzet@gmail.com>
2015-09-19 20:29:36 +0300

    Verbose logging for DB queries

--- src/org/ankhzet/ergo/db/DB.java
+++ src/org/ankhzet/ergo/db/DB.java
@@ -1,12 +1,19 @@
 package org.ankhzet.ergo.db;
 
+import org.ankhzet.ergo.Config;
+import org.ankhzet.ergo.classfactory.annotations.DependencyInjection;
 import org.ankhzet.ergo.db.query.Builder;
+import org.ankhzet.ergo.ui.UILogic;
+import org.ankhzet.ergo.utils.Strings;
 
 /**
  *
  * @author Ankh Zet (ankhzet@gmail.com)
  */
 public class DB extends Builder {
+
+  @DependencyInjection()
+  protected Config config;
 
   public DB() {
     super();
@@ -18,7 +25,29 @@ public class DB extends Builder {
 
   @Override
   public String beforeSQL(String sql) {
+    if (config.get("db.log", false))
+      logSQL(sql);
+
     return sql;
+  }
+
+  void logSQL(String sql) {
+    StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+    Strings filtered = new Strings();
+
+    for (StackTraceElement element : stack) {
+      if (element.isNativeMethod() || element.getLineNumber() < 0)
+        continue;
+
+      String c = element.getClassName();
+      if (!c.contains(".ergo.") || c.contains(".db.") || c.contains(".classfactory."))
+        continue;
+
+      filtered.add(element.toString());
+    }
+
+    UILogic.log("SQL => %s \n\t[ %s\n\t]", subtitutedSQL(sql), filtered.join("\n\t| "));
   }
 
 }
